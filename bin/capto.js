d30d9eb974d294f9d1e5ace23e18f1d2c50bbfba
Jamie Hall <hall@ill.eu>
2014-10-30 15:22:28 +0100

    Implemented CLI entry point.

--- bin/capto.js
+++ bin/capto.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 
 var pkg = require('../package.json'),
-    version = pkg.version,
-    program = require('commander');
-
+  version = pkg.version,
+  program = require('commander');
+path = require('path'),
+  settings = require(path.resolve(__dirname, '../app/config/settings')),
+  Server = require(path.resolve(__dirname, '../server.js'));
 program
   .version('0.0.1', '--version')
   .usage('[options]')
-  .option('--ip [address]', 'Set the ip address for both servers', '127.0.0.1')
-  .option('--smtp-port [port]', 'Set the port of the smtp server', 9025)
-  .option('--http-port [port]', 'Set the port of the http server', 9024)
-  .option('--max-message-size [size]', 'Set the max message size the smtp server will accept in bytes', 20000000)
+  .option('--smtp-ip [address]', 'Set the ip address for the http server', settings.smtp.ip)
+  .option('--http-ip [address]', 'Set the ip address for the smtp server', settings.http.ip)
+  .option('--smtp-port [port]', 'Set the port of the smtp server', settings.smtp.port)
+  .option('--http-port [port]', 'Set the port of the http server', settings.http.port)
+  .option('--max-message-size [size]', 'Set the max message size the smtp server will accept in bytes', settings.smtp.maxMessageSize)
   .parse(process.argv);
 
+
+var smtpPort = program.smtpPort,
+  httpPort = program.httpPort,
+  httpIp = program.httpIp,
+  smtpIp = program.smtpIp,
+  maxMessageSize = program.maxMessageSize;
+
+if (smtpPort === httpPort) {
+  console.error('SMTP and HTTP ports cannot be the same');
+  return;
+}
+/**
+ * Instantiate new server instance
+ */
+new Server(httpPort, httpIp, smtpPort, smtpIp, maxMessageSize);
---
+++ server.js
@@ -0,0 +1,108 @@
+function Server(httpPort, httpIp, smtpPort, smtpIp, maxMessageSize) {
+  var express = require('express'),
+    path = require('path'),
+    routes = require('./app/config/routes'),
+    logger = require('./app/services/logger'),
+    smtp = require('smtp-protocol'),
+    models = require('./app/models/'),
+    settings = require('./app/config/settings'),
+    request = require('request'),
+    util = require('util');
+
+  var app = express();
+  app.use(function (req, res, next) {
+    var data = '';
+    req.setEncoding('utf8');
+    req.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    req.on('end', function () {
+      req.body = data;
+      next();
+    });
+  });
+
+// view engine setup
+  app.set('views', path.join(__dirname, '/app/views'));
+  app.set('view engine', 'jade');
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(function (req, res, next) {
+    models(settings, function (err, db) {
+      if (err) return next(err);
+
+      req.models = db.models;
+      req.db = db;
+
+      return next();
+    });
+  });
+  app.use(routes);
+
+
+// catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+// development error handler
+// will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  /**
+   * production error handler
+   * no stacktraces leaked to user
+   */
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+
+  app.set('port', process.env.PORT || 9024);
+
+  var server = app.listen(httpPort, smtpIp, function () {
+    logger.http.info('Express server listening on port %d and address %s', server.address().port, server.address().address);
+  });
+
+  var smtpServer = smtp.createServer(function (req) {
+    var emailData = '';
+
+    req.on('message', function (stream, ack) {
+      ack.accept();
+      stream.on('data', function (chunk) {
+        emailData += chunk;
+      });
+
+      stream.on('end', function () {
+        logger.smtp.info('Received message from: %s (%d)', req.from, emailData.length);
+        if (emailData.length > maxMessageSize) {
+          logger.smtp.error('Rejected message from: %s (%d) because it exceeds the maximum size of %d', req.from, emailData.length, 200000);
+          return;
+        }
+        request.post({
+          url: util.format('http://%s:%d/messages', httpIp, httpPort),
+          body: emailData
+        });
+      });
+    });
+  });
+
+  smtpServer.listen(smtpPort, smtpIp, function () {
+    logger.smtp.info('SMTP server listening on port %d and address %s', smtpServer.address().port, smtpServer.address().address);
+  });
+}
+
+module.exports = Server;
